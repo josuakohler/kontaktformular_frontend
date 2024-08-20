34af4cf874e89b7cee14957d0fd6ec241e9ffdd7
josuakohler <josua.kohler05@gmail.com>
2024-08-20 15:47:41 +0200

    simple frontend

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import KontaktForm from "./components/KontaktForm.vue";
+const app = createApp(App);
+app.component("kontakt-form-comp", KontaktForm);
+app.mount("#app");
