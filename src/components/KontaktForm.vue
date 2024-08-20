6f43a390eb39bf23715883bf1c235d2fce599636
josuakohler <josua.kohler05@gmail.com>
2024-08-20 15:47:41 +0200

    kontaktform component

---
+++ src/components/KontaktForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="name-input">
+            <input type="text" placeholder="firstname">
+            <input type="text" placeholder="lastname">
+        </div>
+
+
+    </div>
+
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
